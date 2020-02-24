8ef7d58473f4e0aee623d26ef9b6a1d007e3e28a
Maria Iulia Severin <55393346+S00201400@users.noreply.github.com>
2020-02-24 09:29:28 +0000

    Loading Spinner Added

--- screens/IncomeOverviewScreen.js
+++ screens/IncomeOverviewScreen.js
@@ -12,68 +12,69 @@ import { deleteInput } from '../helpers/db';
 const IncomeOverviewScreen = props => {
 
 
-  
-  // const [isRefreshing, setIsRefreshing]= useState(false);
-  // const [error, setError] = useState(false);
-  // const [isLoading, setIsLoading] = useState();
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState();
   //IncomeOverviewScreen is taking the data that is in userIncomes 
   //using the useSelector which has the state=initialState : { all parameters for income }
   const inputs = useSelector(state => state.inputs.inputs)
 
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(InputsActions.loadInputs());
-  }, [dispatch]);
-  //   const loadIncomes = useCallback(async () => {
-  //     setError(null);
-  //     setIsRefreshing(true);
-  //     try {
-  //       await dispatch(IncomesActions.fetchIncomes());
-  //     } catch (err) {
-  //       setError(err.message);
-  //     }
-  //  setIsRefreshing(false);
-  //   }, [dispatch, setIsLoading, setError]);
+
+  // useEffect(() => {
+  //   dispatch(InputsActions.loadInputs());
+  // }, [dispatch]);
+  const loadInputs = useCallback(async () => {
+    setError(null);
+    setIsRefreshing(true);
+    try {
+      await dispatch(InputsActions.loadInputs());
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsRefreshing(false);
+  }, [dispatch, setIsLoading, setError]);
 
   //willFocus fire when the transition beginns
   //we should consider this because you want yo load everyting form your db each time
   //not only one time when you enter
-  // useEffect(() => {
-  //   const willFocusSub = props.navigation.addListener('willFocus', loadIncomes);
-  //   return () => {
-  //     willFocusSub.remove();
-  //   };
-  // }, [loadIncomes]);
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener('willFocus', loadInputs);
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [loadInputs]);
 
   //everytime i m loading the screen i want to fire this effect
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   loadIncomes().then(()=>{
-  //     setIsLoading(false);
-  //   });
-  // }, [dispatch, loadIncomes]);
+  useEffect(() => {
+    setIsLoading(true);
+    loadInputs().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, loadInputs]);
 
-  // useEffect(() => {
-  //   if (error) {
-  //     Alert.alert('An error occurred!', error, [{ text: 'Okay' }]);
-  //   }
-  // }, [error]);
+  useEffect(() => {
+    if (error) {
+      Alert.alert('An error occurred!', error, [{ text: 'Okay' }]);
+    }
+  }, [error]);
 
   const deleteHandler = (id) => {
     Alert.alert("Are you sure?", "Do you really want to delete this item?", [
       { text: 'No', style: 'default' },
       {
         text: 'Yes', style: 'destructive', onPress: async () => {
-          // setError(null);
-          //    setIsLoading(true);
-          // try {
-          await dispatch(InputsActions.deleteInput(id));
-          props.navigation.goBack();
-          // } catch (err) {
-          //  setError(err.message);
-          // }
+          setError(null);
+          setIsLoading(true);
+          try {
+            await dispatch(InputsActions.deleteInput(id));
+            props.navigation.goBack();
+          } catch (err) {
+            setError(err.message);
+          }
 
-          //setIsLoading(false);
+          setIsLoading(false);
 
         }
       }
@@ -93,36 +94,36 @@ const IncomeOverviewScreen = props => {
   //   });
   // };
 
-  // if (error) {
-  //   return (
-  //     < View style={styles.centered} >
-  //       <Text>An error occured</Text>
-  //       <Button title="Try again" onPress={loadIncomes} color={Colors.primary} />
-  //     </View >
-  //   );
-  // }
+  if (error) {
+    return (
+      < View style={styles.centered} >
+        <Text>An error occured</Text>
+        <Button title="Try again" onPress={loadInputs} color={Colors.primary} />
+      </View >
+    );
+  }
 
 
   //spinner for loading 
-  // if (isLoading) {
-  //   return (
-  //     < View style={styles.centered} >
-  //       <ActivityIndicator size='large' color={Colors.primary} />
-  //     </View >
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      < View style={styles.centered} >
+        <ActivityIndicator size='large' color={Colors.primary} />
+      </View >
+    );
+  }
 
-  // if (!isLoading && inputs.length === 0) {
-  //   return (
-  //     <View style={styles.centered}>
-  //       <Text>No incomes found. </Text>
-  //     </View>
-  //   );
-  // }
+  if (!isLoading && inputs.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No incomes found. </Text>
+      </View>
+    );
+  }
 
   return <FlatList
-    // onRefresh={loadIncomes}
-    // refreshing={isRefreshing}
+    onRefresh={loadInputs}
+    refreshing={isRefreshing}
     data={inputs}
     keyExtractor={item => item.id}
     renderItem={itemData =>
